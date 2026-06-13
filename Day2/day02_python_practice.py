4130fc939d4e29f3bfa3c4e920d1aaa081006e04
saloniagalawe2006-boop <saloniagalawe2006@gmail.com>
2026-06-13 16:12:04 +0530

    day2

---
+++ Day2/day02_python_practice.py
@@ -0,0 +1,42 @@
+name = ("Saloni")
+print(name)
+print(type(name))
+print(type(20))
+print(type(12.4))
+
+score=10
+print(f"Start={score}")
+score=100
+print(f"updated={score}")
+score=score+5
+print(f"After +5={score}")
+x, y, z = 12,23,34
+print(x,y,x)
+a=b=c=0
+print(a,b,c)
+first_name= "Sallo"
+age= 20
+city="panvel"
+print(type(first_name))
+print(type(city))
+print(type(age))
+print(first_name,age,city)
+num_str="42"
+print(type(num_str))
+pi=3.99
+print(int(pi))
+pi=3.14
+MAX= 100
+print(f"pi= {pi},Max score= {MAX}")
+print("="*40)
+print("              Student INFO")
+print("="*40)
+student_name = "Saloni"
+student_age= 20
+student_city= "Panvel"
+student_occupation="Unemployed"
+#print(student_name,student_age,student_city,student_occupation)
+print(f"Name= {student_name}")
+print(f"Age= {student_age}")
+print(f"City= {student_city}")
+print(f"occupation= {student_occupation}")
